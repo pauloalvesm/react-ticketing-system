94f3b5c221819ecee8654467f7cadcc71ec3679c
pauloalvesm <paulo.alves7351@gmail.com>
2025-11-25 20:32:28 -0300

    feat: apply route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes";
+
 import "./App.css";
 
 export default function App() {
   const [count] = useState(0)
 
   return (
-    <></>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   );
 }

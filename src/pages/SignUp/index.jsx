224a538e8c6256b567d40d2e84cafea16e8590dd
pauloalvesm <paulo.alves7351@gmail.com>
2025-11-26 20:32:01 -0300

    feat: add registration page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,42 @@
 export default function SignUp() {
     return (
-        <div>
-            <h1>Registration Page</h1>
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Ticket system logo" />
+                </div>
+
+                <form>
+                    <h1>New account</h1>
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+
+                    <input
+                        type="text"
+                        placeholder="email@email.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="**********"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button type="submit">Register</button>
+                </form>
+
+                <Link to="/">
+                    Already have an account? Log in
+                </Link>
+
+            </div>
         </div>
     );
 }

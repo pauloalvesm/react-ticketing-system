be7ec0dad38c20425d14483bbbe8190eb45e65a9
pauloalvesm <paulo.alves7351@gmail.com>
2025-11-28 20:39:51 -0300

    feat: set the login page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import "./signin.css";
+
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+
 export default function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div>
-            <h1>login page</h1>
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Call system logo" />
+                </div>
+
+                <form>
+                    <h1>Enter</h1>
+                    <input
+                        type="text"
+                        placeholder="email@email.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="**********"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button type="submit">Access</button>
+                </form>
+
+                <Link to="/register">
+                    Create an account
+                </Link>
+
+            </div>
         </div>
     );
 }

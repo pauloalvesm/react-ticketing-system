b346a893205a20a82abf88741f1c220cd3e6d02d
pauloalvesm <paulo.alves7351@gmail.com>
2025-12-02 21:12:12 -0300

    feat: add user registration function

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,14 +1,54 @@
 import { useState, createContext, useEffect } from "react";
+import { auth, db } from "../services/firebaseConnection";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext({});
 
 export default function AuthProvider({ children }) {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadingAuth] = useState(false);
+    const navigate = useNavigate();
 
     function signIn(email, password) {
         console.log(email)
         console.log(password);
-        alert("SUCCESSFUL LOGIN")
+        alert("SUCCESSFUL LOGIN");
+    }
+
+    async function signUp(email, password, name) {
+        setLoadingAuth(true);
+
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then(async (value) => {
+                let uid = value.user.uid
+
+                await setDoc(doc(db, "users", uid), {
+                    nome: name,
+                    avatarUrl: null
+                })
+                    .then(() => {
+                        let data = {
+                            uid: uid,
+                            nome: name,
+                            email: value.user.email,
+                            avatarUrl: null
+                        };
+                        setUser(data);
+                        storageUser(data);
+                        setLoadingAuth(false);
+                        navigate("/dashboard");
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoadingAuth(false);
+            });
+    }
+
+    function storageUser(data) {
+        localStorage.setItem("@ticketsPRO", JSON.stringify(data))
     }
 
     return (
@@ -16,7 +56,9 @@ export default function AuthProvider({ children }) {
             value={{
                 signed: !!user,
                 user,
-                signIn
+                signIn,
+                signUp,
+                loadingAuth
             }}
         >
             {children}

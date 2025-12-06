a55da28fd1ad8ed731b2a56c53fa4d7f4d99c800
pauloalvesm <paulo.alves7351@gmail.com>
2025-12-06 11:50:29 -0300

    feat: implement the load and logout user methods

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from "react";
 import { auth, db } from "../services/firebaseConnection";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -10,7 +10,23 @@ export const AuthContext = createContext({});
 export default function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
+
     const navigate = useNavigate();
+
+    useEffect(() => {
+        async function loadUser() {
+            const storageUser = localStorage.getItem("@ticketsPRO");
+
+            if (storageUser) {
+                setUser(JSON.parse(storageUser))
+                setLoading(false);
+            }
+
+            setLoading(false);
+        }
+        loadUser();
+    }, []);
 
     async function signIn(email, password) {
         setLoadingAuth(true);
@@ -32,13 +48,13 @@ export default function AuthProvider({ children }) {
                 setUser(data);
                 storageUser(data);
                 setLoadingAuth(false);
-                toast.success("Bem-vindo(a) de volta!");
+                toast.success("Welcome back!!");
                 navigate("/dashboard");
             })
             .catch((error) => {
                 console.log(error);
                 setLoadingAuth(false);
-                toast.error("Ops algo deu errado!");
+                toast.error("Something went wrong!");
             });
 
     }
@@ -78,6 +94,12 @@ export default function AuthProvider({ children }) {
         localStorage.setItem("@ticketsPRO", JSON.stringify(data))
     }
 
+    async function logout() {
+        await signOut(auth);
+        localStorage.removeItem("@ticketsPRO");
+        setUser(null);
+    }
+
     return (
         <AuthContext.Provider
             value={{
@@ -85,7 +107,9 @@ export default function AuthProvider({ children }) {
                 user,
                 signIn,
                 signUp,
-                loadingAuth
+                loadingAuth,
+                logout,
+                loading
             }}
         >
             {children}
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import {AuthContext} from "../../contexts/auth";
+
 export default function Dashboard() {
+    const { logout } = useContext(AuthContext);
+
+    async function handleLogout() {
+        await logout();
+    }
+
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Page Dashboard</h1>
+            <button onClick={handleLogout}>Log out</button>
         </div>
     );
 }

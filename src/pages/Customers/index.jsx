b215b4400129159d4b5a209a48917020053ab3c9
pauloalvesm <paulo.alves7351@gmail.com>
2025-12-14 20:45:55 -0300

    feat: create function to register customer

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -2,16 +2,39 @@ import { useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { FiUser } from "react-icons/fi";
+import { db } from "../../services/firebaseConnection";
+import { addDoc, collection } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 export default function Customers() {
-    const [nome, setName] = useState("");
+    const [name, setName] = useState("");
     const [cnpj, setCnpj] = useState("");
     const [address, setAddress] = useState("");
 
-    function handleRegister(e) {
+    async function handleRegister(e) {
         e.preventDefault();
-        alert("TEST")
-    }
+
+        if (name !== '' && cnpj !== '' && address !== '') {
+            await addDoc(collection(db, "customers"), {
+                companyAddress: name,
+                cnpj: cnpj,
+                address: address
+            })
+                .then(() => {
+                    setName('')
+                    setCnpj('')
+                    setAddress('')
+                    toast.success("Registered company!")
+                })
+                .catch((error) => {
+                    console.log(error);
+                    toast.error("Error while registering.")
+                })
+
+        } else {
+            toast.error("Fill in all fields!")
+        }
+  }
 
     return (
         <div>

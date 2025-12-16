aed1c2c7f0129abd28ae3b6c80e3e249d2435ea3
pauloalvesm <paulo.alves7351@gmail.com>
2025-12-15 21:37:20 -0300

    feat: create elements for the dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,10 @@
 import { useContext } from "react";
 import {AuthContext} from "../../contexts/auth";
 import Header from "../../components/Header";
+import Title from "../../components/Title";
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import "./dashboard.css";
 
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
@@ -12,8 +16,72 @@ export default function Dashboard() {
     return (
         <div>
             <Header/>
-            <h1>Page Dashboard</h1>
-            <button onClick={handleLogout}>Log out</button>
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#FFF" size={25} />
+                        New call 
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Customer</th>
+                                <th scope="col">Subject</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Registering at</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Customer">Corner Market</td>
+                                <td data-label="Subject">Support</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{ backgroundColor: '#999' }}>
+                                        Open
+                                    </span>
+                                </td>
+                                <td data-label="Registered">01/28/2025</td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                        <FiSearch color='#FFF' size={17} />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 color='#FFF' size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+
+
+
+                            <tr>
+                                <td data-label="Customer">TECH Information Technology</td>
+                                <td data-label="Subject">Support</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{ backgroundColor: '#999' }}>
+                                        Em aberto
+                                    </span>
+                                </td>
+                                <td data-label="Registered">01/28/2025</td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                        <FiSearch color='#FFF' size={17} />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 color='#FFF' size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+
+            </div>
         </div>
     );
 }
